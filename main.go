f9a22b13b0d9a71bcd6325f819cbb47344ebd82c
Kyr Nastagunin <kirill.nastagunin@gmail.com>
2025-04-30 22:03:52 -0500

    added a bunch of endpoints

--- main.go
+++ main.go
@@ -1,6 +1,345 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"github.com/kyrnas/chirpy/internal/auth"
+	"github.com/kyrnas/chirpy/internal/database"
+	_ "github.com/lib/pq"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	profanities    map[string]bool
+	queries        *database.Queries
+	platform       string
+	jwtSecret      string
+}
+
+type CreateChirpRequest struct {
+	Body string `json:"body"`
+}
+
+type ChirpReponse struct {
+	Id        uuid.UUID `json:"id"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserId    uuid.UUID `json:"user_id"`
+}
+
+type UserRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserReponse struct {
+	Id           uuid.UUID `json:"id"`
+	CreatedAt    string    `json:"created_at"`
+	UpdatedAt    string    `json:"updated_at"`
+	Email        string    `json:"email"`
+	Token        string    `json:"token"`
+	RefreshToken string    `json:"refresh_token"`
+}
+
+type RefreshTokenResponse struct {
+	Token string `json:"token"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(rw, req)
+	})
+}
+
+func (cfg *apiConfig) middlewareAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		token, err := auth.GetBearerToken(req.Header)
+		if err != nil {
+			rw.WriteHeader(401)
+			json.NewEncoder(rw).Encode((ErrorResponse{Error: "Token not present"}))
+			return
+		}
+		_, err = auth.ValidateJWT(token, cfg.jwtSecret)
+		if err != nil {
+			rw.WriteHeader(401)
+			json.NewEncoder(rw).Encode((ErrorResponse{Error: "Failed to validate token"}))
+			return
+		}
+		next.ServeHTTP(rw, req)
+	})
+}
+
+func (cfg *apiConfig) getChirpsHandler(rw http.ResponseWriter, req *http.Request) {
+	chirps, err := cfg.queries.GetAllChirps(req.Context())
+	if err != nil {
+		rw.WriteHeader(500)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Internal server error"})
+		return
+	}
+	var chirpResponse []ChirpReponse
+
+	for _, chirp := range chirps {
+		chirpResponse = append(chirpResponse, ChirpReponse{
+			Id:        chirp.ID,
+			CreatedAt: chirp.CreatedAt.Time.String(),
+			UpdatedAt: chirp.UpdatedAt.Time.String(),
+			Body:      chirp.Body,
+			UserId:    chirp.UserID,
+		})
+	}
+
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.WriteHeader(200)
+	json.NewEncoder(rw).Encode(chirpResponse)
+}
+
+func (cfg *apiConfig) getChirpHandler(rw http.ResponseWriter, req *http.Request) {
+	chirpIDString := req.PathValue("chirpID")
+	if chirpIDString == "" {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Chirp ID is required"})
+		return
+	}
+	chirpID, err := uuid.Parse(chirpIDString)
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid Chirp ID"})
+		return
+	}
+	chirp, err := cfg.queries.GetChirp(req.Context(), chirpID)
+	if err != nil {
+		rw.WriteHeader(404)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Not found"})
+		return
+	}
+
+	chirpResponse := ChirpReponse{
+		Id:        chirp.ID,
+		CreatedAt: chirp.CreatedAt.Time.String(),
+		UpdatedAt: chirp.UpdatedAt.Time.String(),
+		Body:      chirp.Body,
+		UserId:    chirp.UserID,
+	}
+
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.WriteHeader(200)
+	json.NewEncoder(rw).Encode(chirpResponse)
+}
+
+func (cfg *apiConfig) createChirpHandler(rw http.ResponseWriter, req *http.Request) {
+	var chirpRequest CreateChirpRequest
+	err := json.NewDecoder(req.Body).Decode(&chirpRequest)
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid chirp"})
+		return
+	}
+	if len(chirpRequest.Body) > 140 {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Chirp is too long"})
+		return
+	}
+	words := strings.Fields(chirpRequest.Body)
+	for i, word := range words {
+		if cfg.profanities[strings.ToLower(word)] {
+			words[i] = "****"
+		}
+	}
+
+	token, _ := auth.GetBearerToken(req.Header)
+	userId, _ := auth.ValidateJWT(token, cfg.jwtSecret)
+
+	dbChirp, err := cfg.queries.CreateChirp(req.Context(), database.CreateChirpParams{
+		UserID: userId,
+		Body:   strings.Join(words, " "),
+	})
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid chirp"})
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.WriteHeader(201)
+
+	chirpResponse := ChirpReponse{
+		Id:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt.Time.String(),
+		UpdatedAt: dbChirp.UpdatedAt.Time.String(),
+		Body:      dbChirp.Body,
+		UserId:    dbChirp.UserID,
+	}
+	json.NewEncoder(rw).Encode(chirpResponse)
+}
+
+func (cfg *apiConfig) metricsHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte(fmt.Sprintf(
+		`
+			<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+			</html>
+		`,
+		int(cfg.fileserverHits.Load()),
+	)))
+}
+
+func (cfg *apiConfig) resetHandler(rw http.ResponseWriter, req *http.Request) {
+	if cfg.platform != "dev" {
+		rw.WriteHeader(403)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Forbidden"})
+		return
+	}
+	cfg.fileserverHits.Store(0)
+	cfg.queries.DeleteAllUsers(req.Context())
+	rw.WriteHeader(200)
+}
+
+func (cfg *apiConfig) createUserHandler(rw http.ResponseWriter, req *http.Request) {
+	var requestBody UserRequest
+	err := json.NewDecoder(req.Body).Decode(&requestBody)
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid request"})
+		return
+	}
+	hashedPassword, err := auth.HashPassword(requestBody.Password)
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Issue hashing password"})
+		return
+	}
+	dbUser, err := cfg.queries.CreateUser(req.Context(), database.CreateUserParams{
+		Email:          requestBody.Email,
+		HashedPassword: hashedPassword,
+	})
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid request"})
+		return
+	}
+	user := UserReponse{
+		Id:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt.Time.String(),
+		UpdatedAt: dbUser.UpdatedAt.Time.String(),
+		Email:     dbUser.Email,
+	}
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.WriteHeader(201)
+	json.NewEncoder(rw).Encode(user)
+}
+
+func (cfg *apiConfig) loginUserHandler(rw http.ResponseWriter, req *http.Request) {
+	var requestBody UserRequest
+	err := json.NewDecoder(req.Body).Decode(&requestBody)
+	if err != nil {
+		rw.WriteHeader(400)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid request"})
+		return
+	}
+	hashedPassword, err := cfg.queries.GetUserHashedPasswordByEmail(req.Context(), requestBody.Email)
+	if err != nil {
+		rw.WriteHeader(401)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "User not found"})
+		return
+	}
+	err = auth.CheckPasswordHash(hashedPassword, requestBody.Password)
+	if err != nil {
+		rw.WriteHeader(401)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Invalid password"})
+		return
+	}
+	tokenExpiration := time.Hour
+
+	dbUser, _ := cfg.queries.GetUserByEmail(req.Context(), requestBody.Email)
+	token, err := auth.MakeJWT(dbUser.ID, cfg.jwtSecret, tokenExpiration)
+	if err != nil {
+		rw.WriteHeader(500)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Unable to create JWT token"})
+		return
+	}
+
+	refreshToken, _ := auth.MakeRefreshToken()
+
+	cfg.queries.CreateRefreshToken(req.Context(), database.CreateRefreshTokenParams{
+		Token:     refreshToken,
+		UserID:    dbUser.ID,
+		ExpiresAt: time.Now().Add(60 * 24 * time.Hour),
+	})
+
+	user := UserReponse{
+		Id:           dbUser.ID,
+		CreatedAt:    dbUser.CreatedAt.Time.String(),
+		UpdatedAt:    dbUser.UpdatedAt.Time.String(),
+		Email:        dbUser.Email,
+		Token:        token,
+		RefreshToken: refreshToken,
+	}
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	rw.WriteHeader(200)
+	json.NewEncoder(rw).Encode(user)
+}
+
+func (cfg *apiConfig) refreshTokenHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+	refreshToken, err := auth.GetBearerToken(req.Header)
+	if err != nil {
+		rw.WriteHeader(401)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Token not present"})
+		return
+	}
+
+	refreshEntity, err := cfg.queries.GetRefreshToken(req.Context(), refreshToken)
+	if err != nil || refreshEntity.RevokedAt.Valid || refreshEntity.ExpiresAt.Before(time.Now()) {
+		rw.WriteHeader(401)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Token is invalid"})
+		return
+	}
+
+	newToken, err := auth.MakeJWT(refreshEntity.UserID, cfg.jwtSecret, time.Hour)
+	if err != nil {
+		rw.WriteHeader(500)
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Failed to create a new token"})
+		return
+	}
+
+	refreshResponse := RefreshTokenResponse{Token: newToken}
+	rw.WriteHeader(200)
+	json.NewEncoder(rw).Encode(refreshResponse)
+}
+
+func (cfg *apiConfig) revokeTokenHandler(rw http.ResponseWriter, req *http.Request) {
+	refreshToken, err := auth.GetBearerToken(req.Header)
+	if err != nil {
+		rw.WriteHeader(401)
+		rw.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(ErrorResponse{Error: "Token not present"})
+		return
+	}
+
+	cfg.queries.ExpireRefreshToken(req.Context(), refreshToken)
+	rw.WriteHeader(204)
+}
 
 func healthHandler(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -9,9 +348,37 @@ func healthHandler(rw http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+
+	dbQueries := database.New(db)
+
+	platform := os.Getenv("PLATFORM")
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	apiCfg := &apiConfig{fileserverHits: atomic.Int32{}, profanities: map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}, queries: dbQueries, platform: platform, jwtSecret: jwtSecret}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", healthHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.Handle("POST /api/chirps", apiCfg.middlewareAuth(http.HandlerFunc(apiCfg.createChirpHandler)))
+	mux.HandleFunc("GET /api/chirps", apiCfg.getChirpsHandler)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.getChirpHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.createUserHandler)
+	mux.HandleFunc("POST /api/login", apiCfg.loginUserHandler)
+	mux.HandleFunc("POST /api/refresh", apiCfg.refreshTokenHandler)
+	mux.HandleFunc("POST /api/revoke", apiCfg.revokeTokenHandler)
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
